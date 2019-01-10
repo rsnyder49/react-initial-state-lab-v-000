d032d65388660d69957e42c69f563838f649a903
Robert Snyder <r.snyder49@gmail.com>
2019-01-10 00:48:06 +0000

    Automatically backed up by Learn

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
- export default class Bomb extends Component {
+export default class Bomb extends Component {
 
    constructor(props) {
     super(props)
